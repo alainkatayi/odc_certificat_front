261449113d7e02873eb68e744ba31893001efebb
alainkatayi <alainkatayi01@gmail.com>
2025-10-16 19:09:10 +0100

    Ajout de la pagination dans la liste des certificat

--- src/app/pages/admin/certificat-list/certificat-list.component.ts
+++ src/app/pages/admin/certificat-list/certificat-list.component.ts
@@ -4,6 +4,7 @@ import { CertificatService } from '../../../core/services/certificat/certificat.
 import { Certificat } from '../../../core/models/certificat';
 import { environnement } from '../../../../environnemnts/environnement';
 import { CommonModule } from '@angular/common';
+import { PaginationLink, PaginationMeta, PaginationUrls } from '../../../core/models/pagination';
 
 @Component({
   selector: 'app-certificat-list',
@@ -15,6 +16,9 @@ export class CertificatListComponent {
   certificats!:Certificat[]
   url = environnement.Url
   isLoadingCertificat:Boolean = false
+  paginationMeta:PaginationMeta | null = null
+  currentPage:number = 1
+  links!: PaginationUrls
   
 
   constructor( private certificatService:CertificatService){
@@ -22,21 +26,34 @@ export class CertificatListComponent {
   }
 
   ngOnInit(){
-    this.getCertificats()
+    this.loadCertificats(this.currentPage)
   }
 
-  getCertificats(){
+  loadCertificats(page:number){
     this.isLoadingCertificat = true
-    this.certificatService.getCertificats().subscribe({
+    this.certificatService.getCertificats(page).subscribe({
       next:(response)=>{
         this.isLoadingCertificat = false
         this.certificats = response.data
         console.log(this.certificats)
+        this.paginationMeta = response.meta
+        this.links = response.links
+        this.currentPage = response.meta.current_page
       },
       error:(error)=>{
         this.isLoadingCertificat = false
         console.log(error)
       }
     })
+  }
+
+  onPageClick(link: PaginationLink) {
+      if (!link.url) return;
+  
+      const url = new URL(link.url);
+      const pageParam = url.searchParams.get('page');
+      const page = pageParam ? parseInt(pageParam, 10) : 1;
+  
+      this.loadCertificats(page);
   }
 }
