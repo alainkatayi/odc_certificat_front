c78c1862ab175f5c3e88d43d1e825f1c703fb878
alainkatayi <alainkatayi01@gmail.com>
2025-10-16 16:13:47 +0100

    Création du model certificat

---
+++ src/app/core/models/certificat.ts
@@ -0,0 +1,8 @@
+export interface Certificat {
+    id: number;
+    participants_id: number;
+    formation_id: number;
+    certificat_path: string;
+    created_at: string;
+    updated_at: string;
+}
