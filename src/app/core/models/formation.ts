7dce09cccc7da340fcc8efa364f2a3203e017bc5
alainkatayi <alainkatayi01@gmail.com>
2025-10-16 15:54:08 +0100

    Création du model formation

---
+++ src/app/core/models/formation.ts
@@ -0,0 +1,9 @@
+export interface Formation{
+    id:number,
+    name:string,
+    description:string,
+    certificat_file:string,
+    participant_file:string,
+    start_date:string,
+    end_date:string,
+}
