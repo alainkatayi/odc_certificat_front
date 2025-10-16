df1eae21a96b1f8739d7d81156336936173fb29f
alainkatayi <alainkatayi01@gmail.com>
2025-10-16 18:33:13 +0100

    Ajout de la pagination dans la list des participants

--- src/app/pages/admin/participant-list/participant-list.component.ts
+++ src/app/pages/admin/participant-list/participant-list.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { SidebarComponent } from "../../../components/sidebar/sidebar.component";
 import { Participant } from '../../../core/models/participant';
 import { CommonModule } from '@angular/common';
-import { PaginationMeta } from '../../../core/models/pagination';
+import { PaginationLink, PaginationMeta, PaginationUrls } from '../../../core/models/pagination';
 
 @Component({
   selector: 'app-participant-list',
@@ -15,18 +15,29 @@ export class ParticipantListComponent {
   constructor(private participantService: ParticipantService) { }
   participants!: Participant[]
   isLoading: boolean = false
+  paginationMeta:PaginationMeta | null = null
+  currentPage:number = 1
+  links!: PaginationUrls
 
   ngOnInit() {
-    this.loadParticipants()
+    this.loadParticipants(this.currentPage)
   }
 
-  loadParticipants() {
+  loadParticipants(page:number) {
     this.isLoading = true
-    this.participantService.getParticipants().subscribe({
+    this.participantService.getParticipants(page).subscribe({
       next: (response) => {
         console.log(response)
         this.isLoading = false
         this.participants = response.data
+        this.paginationMeta = response.meta
+        this.links = response.links
+        console.log("links",this.links)
+        console.log("response meat",response.meta)
+        console.log("response sans meta",response)
+        console.log("pagination meat",this.paginationMeta)
+        this.currentPage = response.meta.current_page
+        console.log("current page",this.currentPage)
         console.log(this.participants)
    
       },
@@ -35,4 +46,14 @@ export class ParticipantListComponent {
       }
     })
   }
+
+  onPageClick(link: PaginationLink) {
+    if (!link.url) return;
+
+    const url = new URL(link.url);
+    const pageParam = url.searchParams.get('page');
+    const page = pageParam ? parseInt(pageParam, 10) : 1;
+
+    this.loadParticipants(page);
+}
 }
